33cede8a62a5ceb06f9f3fd720919ff6fb588296
Niranjan <niranuv@gmail.com>
2018-07-30 08:16:27 +0530

    Adding Manual Dependency Injection code

---
+++ src/main/java/guru/springframework/services/GreetingServiceImpl.java
@@ -0,0 +1,17 @@
+package guru.springframework.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingServiceImpl implements GreetingService{
+
+	public static final String HELLO_GURUS = "Hello Gurus!!!!";
+	
+	@Override
+	public String sayGreeting() {
+		// TODO Auto-generated method stub
+		return HELLO_GURUS;
+	}
+
+	
+}
